947fca942f39b9729bccdade3fd3252be6c67d49
omykhalska <omykhalska@gmail.com>
2023-08-11 19:45:50 +0200

    add css configs for colors and sizes

---
+++ src/configs/colorConfigs.ts
@@ -0,0 +1,17 @@
+import { colors } from '@mui/material';
+
+const colorConfigs = {
+  mainBg: colors.grey['100'],
+  sidebar: {
+    bg: '#233044',
+    color: '#eeeeee',
+    hoverBg: '#1e293a',
+    activeBg: '#1e253a',
+  },
+  topbar: {
+    bg: '#fff',
+    color: '#000',
+  },
+};
+
+export default colorConfigs;
